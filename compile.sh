e9d02f62b7a09bd9f4e2e4be637cb3720769d877
DANDA SAI KOTI REDDY <d.saikotireddy@gmail.com>
2020-09-15 00:07:11 +0530

    Traffic Google Analytics

--- compile.sh
+++ compile.sh
@@ -12,3 +12,13 @@
 ./jemdoc.py research.jemdoc
 ./jemdoc.py service.jemdoc
 
+# <!-- Global site tag (gtag.js) - Google Analytics -->
+# <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177953153-1"></script>
+# <script>
+#   window.dataLayer = window.dataLayer || [];
+#   function gtag(){dataLayer.push(arguments);}
+#   gtag('js', new Date());
+
+#   gtag('config', 'UA-177953153-1');
+# </script>
+
